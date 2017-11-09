0cdc4b384d214283d6899c2904d7d93127eea1d5
Luiz Tanure <letanure@gmail.com>
2017-11-09 23:11:09 +0100

    Add device sizes options to each url/panel

--- code.js
+++ code.js
@@ -9,6 +9,23 @@ function $$(selector) {
 var iframes = $$("iframe");
 var urls = $$("input");
 
+function resizeIframe () {
+	const screenSizeOption =  this.value
+	const targetIframe = parseInt(this.dataset.targetIframe);
+	let screenSizeValue = []
+	
+	if (screenSizeOption === 'auto') {
+		screenSizeValue = ['100%', '100%'];
+	} else {
+		screenSizeValue = this.value.split('x').map(val => `${val}px`);
+	}
+	
+	iframes[targetIframe].style.width = screenSizeValue[0];
+	iframes[targetIframe].style.height = screenSizeValue[1];
+}
+
+$$(".device_select").forEach(el => el.addEventListener('change', resizeIframe))
+
 $("form").onsubmit = function() {
 	for (let i=0; i < 2; i++) {
 		let iframe = iframes[i];
